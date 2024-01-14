7b40c87494ccf46f1e165e87ad5896bca53620b3
ziyaak <ziya.akberov08@gmail.com>
2024-01-14 14:32:39 +0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,75 @@
+let a = 12;
+
+let b = 2;
+
+let c = 3;
+
+console.log( a + b + c)
+
+console.log( a * b * c)
+
+console.log( a / b / c)
+
+console.log( a**2)
+
+
+let menfi = -5;
+if(menfi<0){
+
+    console.log("Menfi Ededdir")
+}
+else if(menfi>0)
+
+    console.log("Musbet Ededdir")
+
+
+let number = 12;
+
+if( number % 2 == 0){
+    
+    console.log("Cüt Ededdir")
+}
+else if( number % 2 == 1)
+    console.log("Tek Ededdir")
+
+
+let saat = 1;
+if( saat == 12){
+    console.log("isdir")
+}
+
+else if(saat ==13){
+    console.log("isdir")
+}
+else if(saat ==14){
+    console.log("isdir")
+}
+else if(saat ==15){
+    console.log("isdir")
+}
+else if(saat ==16){
+    console.log("isdir")
+}
+else if(saat ==17){
+    console.log("isdir")
+}
+else if(saat ==18){
+    console.log("isdir")
+}
+else if(saat ==19){
+    console.log("isdir")
+}
+
+else(
+    console.log("deil")
+)
+
+let age = 1;
+
+if (age > 17){
+
+    console.log("Siz passport ala bilərsiniz")
+}
+else(
+    console.log("Siz passport ala bilməzsiniz")
+)
